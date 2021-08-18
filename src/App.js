50ba0745cdddb5a193697518d6326c478f634e09
Timofey <fognir742@gmail.com>
2021-08-18 16:39:34 +0800

    added click count for update logic main button

--- src/App.js
+++ src/App.js
@@ -8,6 +8,19 @@ const buttons = [1, 2, 3];
 function App() {
   const [buttonsVisible, setButtonsVisible] = React.useState(false);
   const [buttonValue, setButtonValue] = React.useState(1);
+  const [clickCount, setClickCount] = React.useState(0);
+
+  const clickHandler = (value) => {
+    setClickCount((prev) => prev + 1);
+    setButtonValue(value);
+  };
+
+  React.useEffect(() => {
+    if (clickCount === buttons.length) {
+      setButtonsVisible(false);
+      setClickCount(0);
+    }
+  }, [clickCount]);
 
   return (
     <div className="container">
@@ -20,7 +33,7 @@ function App() {
             <Button
               key={`${button}_${index}`}
               value={button}
-              onClick={() => setButtonValue(button)}
+              onClick={() => clickHandler(button)}
             />
           ))}
         </div>
